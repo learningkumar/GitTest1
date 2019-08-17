fbeb7d7646e18097496bfebc00e2e45f945ad5fe
tiwashet <madkvns007@gmail.com>
2019-08-17 20:11:06 +0530

    added add methos

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -6,5 +6,11 @@ namespace ConsoleApp1
 {
     class Feature1
     {
+        public int Add()
+        {
+            int x1 = 5;
+            int x2 = 10;
+            return x1 + x2;
+        }
     }
 }
